1cd6a557ce2df34d4dd7e0c27cdd14ede25c9ba2
Johnathon Ludeman <jludeman@kabaminc.com>
2016-01-03 22:22:13 -0800

    fix rename error

--- belletto_server/wsgi.py
+++ belletto_server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chat_server project.
+WSGI config for belletto_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belletto_server.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
