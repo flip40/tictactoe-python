5638bf1093bcf7b78a3000d9b1212ba52361028c
Johnathon Ludeman <jludeman@kabaminc.com>
2016-01-30 23:48:44 -0800

    more chat fixes

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from models import Message
 
 # Create your views here.
@@ -13,6 +14,7 @@ def update_chat(request):
 	messages = Message.objects.all()
 	return HttpResponse(serializers.serialize("json", messages), content_type="application/json")
 
+@csrf_exempt
 def send_message(request):
 	if request.method == 'POST':
 		if 'message' in request.POST:
