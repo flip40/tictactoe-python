2bb7f2cca2733e25fe4535f1aa54c9055d206014
Johnathon Ludeman <jludeman@kabaminc.com>
2016-01-31 01:46:27 -0800

    fixing text field to be non unique

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Message(models.Model):
-	text = models.CharField(unique=True, max_length=255)
+	text = models.CharField(unique=False, max_length=255)
 
 	def __unicode__(self):
 		return u'%s: %s' % (self.id, self.text)
